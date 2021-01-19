c9b71372fe9aac074caa7525688a0c080216236e
Ranieri Miranda Imperatori <ranieri.imperatori@gmail.com>
2021-01-19 15:56:19 -0300

    Add Componentes

---
+++ componentes/Titulo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import estilo from './estilo';
+
+export default function Titulo() {
+    return (
+        <View style = { estilo.box }>
+            <Text style = { estilo.texto }>Noticia do dia</Text>
+        </View>
+    );
+ 
+}
